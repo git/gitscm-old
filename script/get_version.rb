d092325986b66b7175f01f678434c75bbc672f9b
Scott Chacon <schacon@gmail.com>
2008-07-24 16:12:45 -0700

    updated script to modify the shared version too

--- script/get_version.rb
+++ script/get_version.rb
@@ -1,11 +1,11 @@
 #! /usr/bin/env ruby
 require 'rubygems'
-require 'mechanize'
-require 'chronic'
+require 'open-uri'
 require 'yaml'
 require 'pp'
 
 version_file = File.expand_path(File.dirname(__FILE__) + "/../config/app_config.yml")
+perm_file = "/u/apps/gitscm/shared/config/app_config.yml"
 
 current_version = nil
 if File.exists?(version_file)
@@ -15,13 +15,15 @@ end
 
 url = 'http://kernel.org/pub/software/scm/git/'
 
-agent = WWW::Mechanize.new
-get_page = agent.get(url)
+page = ''
+open(url) do |f|
+   page = f.read
+end
 
 versions = []
 
 regex = /<a href="git-([\d\.]*).tar.bz2">git-([\d\.]*).tar.bz2<\/a> (.*) ([\d\.]+[MK])/
-matches = get_page.body.scan(regex)
+matches = page.scan(regex)
 matches.each do |match|
   versions << [Time.parse(match[2].strip), match[1], match[3]]
 end
@@ -32,4 +34,5 @@ latest_version = latest[1]
 if latest_version != current_version
   data = {'latest_version' => {'version' => latest[1], 'ts' => latest[0].to_i}}
   File.open(version_file, 'w') { |f| f.write(data.to_yaml) }
+  File.open(perm_file, 'w') { |f| f.write(data.to_yaml) } if File.exists?(perm_file)
 end
