919c70be95938d59fc9c0a55326b3fed3ebfa699
Scott Chacon <schacon@gmail.com>
2008-07-25 17:41:21 -0700

    fixed author counts to some degree - thanks junio

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -11,11 +11,13 @@ class PageController < ApplicationController
       authors = File.readlines(authors)
       @authors = {:main => [], :contrib => []}
       authors.each do |author|
-        number, *name = author.split(' ')
+        data = author.split(' ')
+        puts number = data.pop.gsub('(', '').gsub(')', '').chomp
+        name = data.join(' ')
         if(number.to_i > 50)
-          @authors[:main] << [name.join(' '), number.to_i]
+          @authors[:main] << [name, number.to_i]
         else
-          @authors[:contrib] << [name.join(' '), number.to_i]
+          @authors[:contrib] << [name, number.to_i]
         end
       end
     end
