6b656d57a2da4f51d2d2e0e82c40e551e82dff19
Scott Chacon <schacon@gmail.com>
2008-07-27 10:28:39 -0700

    added deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+set :application, "gitscm"
+set :repository,  "git://github.com/schacon/learn-github.git"
+
+default_run_options[:pty] = true
+
+set :scm, "git"
+set :runner, 'app'
+set :user, 'app'
+
+set :use_sudo, true
+set :branch, 'master'
+set :user, 'app'
+
+set :deploy_via, :remote_cache
+
+set :domain, 'git-scm.com'
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+task :update_config,  :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/*       #{release_path}/config/"
+end
+after  'deploy:update_code',     :update_config
+
+namespace :passenger do
+  desc 'Restart Application'
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, 'passenger:restart'
