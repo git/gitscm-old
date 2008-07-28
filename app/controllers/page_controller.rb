f0e7b3e660877f3c54d4daf90d9ce1031b1a1dcd
Bryce Roney <brycer22@gmail.com>
2008-07-28 16:03:32 +1000

    Moved the get_authors functionality from controller to pseudo-model.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -6,21 +6,7 @@ class PageController < ApplicationController
   end
   
   def about
-    authors =  File.join(RAILS_ROOT, 'config/authors.txt')
-    if File.exists?(authors)
-      authors = File.readlines(authors)
-      @authors = {:main => [], :contrib => []}
-      authors.each do |author|
-        data = author.split(' ')
-        puts number = data.pop.gsub('(', '').gsub(')', '').chomp
-        name = data.join(' ')
-        if(number.to_i > 50)
-          @authors[:main] << [name, number.to_i]
-        elsif (number.to_i >= 5)
-          @authors[:contrib] << [name, number.to_i]
-        end
-      end
-    end
+    @authors = Author.all
   end
   
   def get_version
