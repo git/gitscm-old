5e1d367771277a8ea15de7c2642046ff87d63d20
Scott Chacon <schacon@gmail.com>
2008-07-30 16:28:54 -0700

    removed debugging info

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,7 +7,7 @@ class Author
       @authors = {:main => [], :contrib => []}
       authors.each do |author|
         data = author.split(' ')
-        puts number = data.pop.gsub('(', '').gsub(')', '').chomp
+        number = data.pop.gsub('(', '').gsub(')', '').chomp
         name = data.join(' ')
         if(number.to_i > 50)
           @authors[:main] << [name, number.to_i]
